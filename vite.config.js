e39a18960f0eaa96216ba2acc54ce50ae425c257
ikyptr <rezkisaputra14022@gmail.com>
2025-07-20 18:31:41 +0700

    fix bug path

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: '/',
+  publicDir: 'public',
   build: {
     outDir: 'dist',
     sourcemap: false,
