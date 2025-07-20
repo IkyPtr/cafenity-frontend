7ea8a277b9c8408e2a00ddf3065afefb701a8daa
ikyptr <rezkisaputra14022@gmail.com>
2025-07-20 18:24:55 +0700

    penambahan redirects

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: '/',
-  resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx']
-  },
   build: {
     outDir: 'dist',
     sourcemap: false,
-    assetsDir: 'assets',
     rollupOptions: {
       output: {
         manualChunks: {
@@ -23,14 +18,5 @@ export default defineConfig({
         }
       }
     }
-  },
-  server: {
-    port: 5173,
-    host: true,
-    historyApiFallback: true
-  },
-  preview: {
-    port: 4173,
-    host: true
   }
 })
